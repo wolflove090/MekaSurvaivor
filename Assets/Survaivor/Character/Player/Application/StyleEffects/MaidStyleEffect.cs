b81f9f23eb815dd301f78c3aa11040fa4ca62c41
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2026-03-14 08:37:01 +0900

    メイドスタイルの実装

--- Assets/Survaivor/Character/Player/Application/StyleEffects/MaidStyleEffect.cs
+++ Assets/Survaivor/Character/Player/Application/StyleEffects/MaidStyleEffect.cs
@@ -3,17 +3,20 @@
 /// </summary>
 public class MaidStyleEffect : IPlayerStyleEffect
 {
+    const float MOVE_SPEED_MULTIPLIER = 1.5f;
+
     /// <summary>
     /// 効果が対応するスタイル種別を取得します
     /// </summary>
     public PlayerStyleType StyleType => PlayerStyleType.Maid;
 
     /// <summary>
-    /// 現時点では追加効果を適用しません
+    /// 効果パラメータ適用時に移動速度倍率を設定します
     /// </summary>
     /// <param name="context">スタイル効果コンテキスト</param>
     public void ApplyParameters(PlayerStyleEffectContext context)
     {
+        context?.PlayerState?.SetMoveSpeedMultiplier(MOVE_SPEED_MULTIPLIER);
     }
 
     /// <summary>
